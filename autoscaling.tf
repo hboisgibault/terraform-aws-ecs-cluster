1fbad88d73437f504744f918516d868cbec8379f
H Boisgibault <h.boisgibault@gmail.com>
2021-07-22 17:20:57 +0200

    Autoscaling group termination policy

--- autoscaling.tf
+++ autoscaling.tf
@@ -49,6 +49,7 @@ resource "aws_autoscaling_group" "main_asg" {
   desired_capacity     = var.target_capacity
   vpc_zone_identifier  = data.aws_subnet_ids.subnets.ids
   wait_for_capacity_timeout = "3m"
+  termination_policy   = "OldestInstance"
 
   instance_refresh {
     strategy = "Rolling"
