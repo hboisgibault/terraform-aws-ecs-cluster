177a361fbed7d928e0cbfed4dacf5af60fe43683
Henry Boisgibault <h.boisgibault@gmail.com>
2022-03-04 20:20:33 +0100

    Typo

--- autoscaling.tf
+++ autoscaling.tf
@@ -36,7 +36,7 @@ resource "aws_launch_configuration" "main_lc" {
   user_data = <<EOF
     #!/bin/bash
     echo ECS_CLUSTER=${ecs_cluster} >> /etc/ecs/ecs.config
-    echo ECS_CONTAINER_INSTANCE_PROPAGATE_TAGS_FROM=ec2_instance>> /etc/ecs/ecs.config
+    echo ECS_CONTAINER_INSTANCE_PROPAGATE_TAGS_FROM=ec2_instance >> /etc/ecs/ecs.config
     ${var.user_data}
   EOF
   
